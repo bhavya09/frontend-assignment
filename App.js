0ca533eda1ba0e930756ebe0bf9ae8215ce62eda
Bhavya Jain <32821623+bhavya09@users.noreply.github.com>
2024-12-15 20:58:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Table from './Table';
+
+function App() {
+
+  return (
+    <div className="App">
+            <Table />
+    </div>
+  );
+}
+
+export default App;
